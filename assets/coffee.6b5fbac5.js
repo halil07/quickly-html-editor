283daca45a1dfe4a0ec21bea436c11f18329553f
halil07 <halilceyhan@yandex.com>
2022-11-30 22:21:15 +0300

    Updates

--- assets/coffee.6b5fbac5.js
+++ assets/coffee.6b5fbac5.js
@@ -1,4 +1,4 @@
-import"./app.f3d5a829.js";const t=`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+import"./app.c5fcc862.js";const t=`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
 	<!--[if gte mso 9]>
